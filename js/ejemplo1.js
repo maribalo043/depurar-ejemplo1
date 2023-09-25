eb95c914ccd743e77c4656a5196e7923952a409c
Mario <mariobl90@educastur.es>
2023-09-25 13:28:48 +0200

    he cambiado el nombre del autor.

--- js/ejemplo1.js
+++ js/ejemplo1.js
@@ -1,6 +1,6 @@
 /**
  * DWEC: Ejemplo 1. Encontrar los errores del script
- * @author Maria R. F.
+ * @author Mario B.L.
  * 
  * Realiza un script que se encargue de leer 10 nombres por pantalla y los inserte en un array. 
  * Posteriormente, los nombres se mostrarán una unica ventana emergente
@@ -9,11 +9,7 @@
 /* Encontrar los errores que tiene el siguiente script, usando las herramientas de depuración de Google Chrome */
 
 var nombres=[];
-
 var NUMERO_NOMBRES=10;
-
-
-
 
 /* AVISOS (WARNING) */
 console.warn("Mensaje warn");
